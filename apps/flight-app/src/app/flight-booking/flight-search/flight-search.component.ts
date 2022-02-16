1c533ad7ae7c1e8a58cf565ccaa1d67950d51b6d
mikezks <23002661+mikezks@users.noreply.github.com>
2022-02-16 14:14:30 +0100

    feat: redux setup, set flight array

--- apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import {Component, OnInit} from '@angular/core';
-import {FlightService} from '@flight-workspace/flight-lib';
+import { FlightService } from '@flight-workspace/flight-lib';
+import { Store } from '@ngrx/store';
+import * as formFlightBooking from '../+state';
 
 @Component({
   selector: 'flight-search',
@@ -13,10 +15,7 @@ export class FlightSearchComponent implements OnInit {
   from = 'Hamburg'; // in Germany
   to = 'Graz'; // in Austria
   urgent = false;
-
-  get flights() {
-    return this.flightService.flights;
-  }
+  flights$ = this.store.select(state => state.flightBooking.flights);
 
   // "shopping basket" with selected flights
   basket: { [id: number]: boolean } = {
@@ -25,7 +24,8 @@ export class FlightSearchComponent implements OnInit {
   };
 
   constructor(
-    private flightService: FlightService) {
+    private flightService: FlightService,
+    private store: Store<formFlightBooking.FlightBookingRootState>) {
   }
 
   ngOnInit() {
@@ -35,7 +35,12 @@ export class FlightSearchComponent implements OnInit {
     if (!this.from || !this.to) return;
 
     this.flightService
-      .load(this.from, this.to, this.urgent);
+      .find(this.from, this.to, this.urgent)
+      .subscribe(
+        flights => this.store.dispatch(
+          formFlightBooking.flightsLoaded({ flights })
+        )
+      );
   }
 
   delay(): void {
